22001a53334724aca3b56be8f686e8f24d81eceb
Mrots27 <yangfengbo68@gmail.com>
2019-10-07 19:02:57 +1100

    加了选图书馆，未实现跳转

--- app/src/main/java/comp5216/sydney/edu/au/betterstudy/ui/home/HomeFragment.java
+++ app/src/main/java/comp5216/sydney/edu/au/betterstudy/ui/home/HomeFragment.java
@@ -1,18 +1,26 @@
 package comp5216.sydney.edu.au.betterstudy.ui.home;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import comp5216.sydney.edu.au.betterstudy.R;
+import comp5216.sydney.edu.au.betterstudy.ui.dashboard.DashboardFragment;
 
 public class HomeFragment extends Fragment {
 
@@ -20,16 +28,18 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        View root = inflater.inflate(R.layout.fragment_library, container, false);
+/*        final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
+
 }
