4eb7dcfacf4bde5ff256b8afcd1e7f7accd3c5f9
Hill <yanhengfeng_cq@163.com>
2019-10-07 23:15:12 +1100

    create Order pages waiting database

--- app/src/main/java/comp5216/sydney/edu/au/betterstudy/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/comp5216/sydney/edu/au/betterstudy/ui/dashboard/DashboardFragment.java
@@ -1,16 +1,24 @@
 package comp5216.sydney.edu.au.betterstudy.ui.dashboard;
 
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import java.util.ArrayList;
 
 import comp5216.sydney.edu.au.betterstudy.R;
 
@@ -18,18 +26,94 @@ public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
+    Button button;
+    TextView noIncomplete;
+    TextView noHistory;
+    ListView incomplete1;
+    ListView incomplete2;
+    ListView historyDate;
+    ListView history1;
+    ListView history2;
+    ArrayList<String> items;
+    ArrayList<String> incompleteItems;
+    ArrayList<String> historyDates;
+    ArrayList<String> historyItems;
+    ArrayAdapter<String> itemsAdapter;
+    ArrayAdapter<String> incompleteItemsAdapter;
+    ArrayAdapter<String> historyDatesAdapter;
+    ArrayAdapter<String> historyItemsAdapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        button = root.findViewById(R.id.cancelorder);
+        noIncomplete = root.findViewById(R.id.noIncomplete);
+        noHistory = root.findViewById(R.id.noHistory);
+        incomplete1 = root.findViewById(R.id.incomplete1);
+        incomplete2 = root.findViewById(R.id.incomplete2);
+        historyDate = root.findViewById(R.id.historyDate);
+        history1 = root.findViewById(R.id.history1);
+        history2 = root.findViewById(R.id.history2);
+
+        items = new ArrayList<String>();
+        incompleteItems = new ArrayList<String>();
+        historyDates = new ArrayList<String>();
+        historyItems = new ArrayList<String>();
+
+        //there should be a method to read database
+
+
+        itemsAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, items);
+        incompleteItemsAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, incompleteItems);
+        historyDatesAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, historyDates);
+        historyItemsAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, historyItems);
+
+        if (incompleteItems.isEmpty()){
+            noIncomplete.setVisibility(View.VISIBLE);
+        }else {
+            noIncomplete.setVisibility(View.INVISIBLE);
+            incomplete1.setAdapter(itemsAdapter);
+            incomplete2.setAdapter(incompleteItemsAdapter);
+        }
+
+        if (historyDates.isEmpty()){
+            noHistory.setVisibility(View.VISIBLE);
+        }else {
+            noHistory.setVisibility(View.INVISIBLE);
+            historyDate.setAdapter(historyDatesAdapter);
+            history1.setAdapter(itemsAdapter);
+            history2.setAdapter( historyItemsAdapter);
+        }
+
+        setupButtonListener();
         return root;
     }
+
+    public void  setupButtonListener(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Cancel this incomplete order")
+                        .setMessage("Do you want to cancel this incomplete order ?")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+    }
+
 }
