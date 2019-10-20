c8963507887d13cf39db1a42f9dc397063129957
lmason <lsmason23@gmail.com>
2019-10-20 15:22:07 +1100

    add function of login and register

--- app/src/main/java/comp5216/sydney/edu/au/betterstudy/MainActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/betterstudy/MainActivity.java
@@ -1,31 +1,26 @@
 package comp5216.sydney.edu.au.betterstudy;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        userId = intent.getStringExtra("ID");
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -35,27 +30,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        isFireBaseConnected();
-    }
-
-    //测试是否能链接数据库
-    public void isFireBaseConnected(){
-        DocumentReference mDocRef = FirebaseFirestore.getInstance().document("User/user1");
-        Map<String,String> dataSave = new HashMap<String,String>();
-        dataSave.put("userId", "123");
-        dataSave.put("userPassWord","321");
-        mDocRef.set(dataSave)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.i("aaa", "Document has been saved");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.i("fff", "Document was not saved",e);
-                    }
-                });
     }
 }
