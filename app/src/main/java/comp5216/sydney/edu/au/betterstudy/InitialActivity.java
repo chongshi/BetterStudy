7d744d3915b89cb00b762ff8160ecb258a3c9025
lmason <lsmason23@gmail.com>
2019-10-22 00:39:40 +1100

    增加实时网络状态判断

--- app/src/main/java/comp5216/sydney/edu/au/betterstudy/InitialActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/betterstudy/InitialActivity.java
@@ -1,19 +1,33 @@
 package comp5216.sydney.edu.au.betterstudy;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class InitialActivity extends AppCompatActivity {
+public class InitialActivity extends AppCompatActivity implements OnClickListener {
 
     private Button btn_getStart;
+    private IntentFilter intentFilter;
+    private NetworkChangReceiver networkChangeReceiver;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+
 
         btn_getStart = findViewById(R.id.ivButton);
         btn_getStart.setOnClickListener(new View.OnClickListener() {
@@ -24,4 +38,30 @@ public class InitialActivity extends AppCompatActivity {
             }
         });
     }
+
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(
+                    Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(getApplicationContext(),
+                        "有网", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "没网", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
 }
